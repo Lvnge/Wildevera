67fec9ff8d1cc4e9a0d08fcb772356d61608fb42
Lvnge <roy9096@hotmail.com>
2026-06-23 16:15:06 -0600

    og redesign

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -23,57 +23,114 @@ export default async function Image() {
   return new ImageResponse(
     <div
       style={{
-        background: "#1F3A34",
+        background: "#F5F2EC",
         width: "100%",
         height: "100%",
         display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: "60px",
+        flexDirection: "row",
       }}
     >
-      <img
-        src={logoBase64}
-        width={120}
-        height={120}
-        style={{ marginBottom: "28px" }}
-      />
+      {/* Left panel */}
       <div
         style={{
-          color: "#C2A46D",
-          fontSize: 28,
-          fontFamily: "Inter",
-          letterSpacing: "0.2em",
-          textTransform: "uppercase",
-          marginBottom: "16px",
+          width: "50%",
+          height: "100%",
+          background: "#F5F2EC",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "60px",
+          borderRight: "1px solid #C2A46D",
         }}
       >
-        WILDEVERA
+        <img
+          src={logoBase64}
+          width={110}
+          height={110}
+          style={{ marginBottom: "24px" }}
+        />
+        <div
+          style={{
+            color: "#1F3A34",
+            fontSize: 64,
+            fontFamily: "Playfair Display",
+            fontWeight: 700,
+            lineHeight: 1,
+            marginBottom: "12px",
+          }}
+        >
+          Wildevera
+        </div>
+        {/* Gold divider */}
+        <div
+          style={{
+            width: "120px",
+            height: "2px",
+            background: "#C2A46D",
+            marginBottom: "16px",
+          }}
+        />
+        <div
+          style={{
+            color: "#8A7F78",
+            fontSize: 22,
+            fontFamily: "Playfair Display",
+            fontStyle: "italic",
+            textAlign: "center",
+          }}
+        >
+          Where conviction meets compassion
+        </div>
       </div>
+
+      {/* Right panel */}
       <div
         style={{
-          color: "#F5F2EC",
-          fontSize: 52,
-          fontFamily: "Playfair Display",
-          fontWeight: 700,
-          textAlign: "center",
-          lineHeight: 1.2,
-          marginBottom: "28px",
+          width: "50%",
+          height: "100%",
+          background: "#1F3A34",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          padding: "60px",
         }}
       >
-        Where Conviction Meets Compassion
-      </div>
-      <div
-        style={{
-          color: "#C2A46D",
-          fontSize: 22,
-          fontFamily: "Inter",
-          textAlign: "center",
-          maxWidth: "700px",
-        }}
-      >
-        Connecting U.S. small businesses with skilled virtual assistants
+        <div
+          style={{
+            color: "#F5F2EC",
+            fontSize: 38,
+            fontFamily: "Playfair Display",
+            fontWeight: 700,
+            lineHeight: 1.3,
+            marginBottom: "24px",
+          }}
+        >
+          Connecting capable people with meaningful work.
+        </div>
+        <div
+          style={{
+            color: "#C2A46D",
+            fontSize: 18,
+            fontFamily: "Inter",
+            letterSpacing: "0.15em",
+            textTransform: "uppercase",
+            marginBottom: "40px",
+          }}
+        >
+          Virtual services & career-building courses
+        </div>
+        <div
+          style={{
+            color: "#8A7F78",
+            fontSize: 18,
+            fontFamily: "Inter",
+            letterSpacing: "0.05em",
+          }}
+        >
+          www.wildevera.com
+        </div>
       </div>
     </div>,
     {
